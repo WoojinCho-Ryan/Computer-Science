14db915364c9b6a4fcef9a80fe0b7e05916414d8
WoojinCho-Ryan <woojincho.cs@gmail.com>
2025-05-10 21:00:25 +0900

    Update String.py

--- Python Studying/Jump To Python /Chapter 2 /2-2/String.py
+++ Python Studying/Jump To Python /Chapter 2 /2-2/String.py
@@ -118,3 +118,30 @@ day = "three"
 # 2. 소수점 표현하기
 "%0.4f" % 3.42134234 # '3.4213'
 "%10.4f" % 3.42134234 # '   3.4213' 공백 4개. 전체 길이 10개.
+
+# format 함수를 사용한 포매팅
+# format 함수를 사용하면 좀 더 발전한 스타일로 문자열 포맷 지정 가능
+# 숫자 바로 대입하기
+"I eat {0} apples".format(3) # 'I eat 3 apples'
+# 문자열 바로 대입하기
+"I eat {0} apples".format("five") # 'I eat five apples'
+# 숫자 값을 가진 변수로 대입하기
+number = 3
+"I eat {0} apples".format(number) # 'I eat 3 apples'
+# 2개 이상의 값 넣기
+number = 10
+day = "three"
+"I ate {0} apples. so I was sick for {1} days.".format(number, day)
+# 이름으로 넣기
+"I ate {number} apples. so I was sick for {day} days.".format(number=10, day=3)
+# 인덱스와 이름을 혼용해서 넣기
+"I ate {0} apples. so I was sick for {day} days.".format(10, day=3)
+# 왼쪽 정렬
+"{0:<10)".format("hi") # 'hi        ' 문자열의 총 자릿수 10.
+# 오른쪽 정렬
+"{0:>10}".format("hi") # '        hi' 문자열의 총 자릿수 10.
+# 가운데 정렬
+"{0:^10}".format("hi") # '    hi    ' 문자열의 총 자릿수 10.
+# 공백 채우기
+"{0:=^10}".format("hi") # '====hi===='
+"{0:!<10}".format("hi") # 'hi!!!!!!!!'
